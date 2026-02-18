636621801eb54bdb664b17d214ae3bb5d5b2b6fc
lleinnadd <daniel@frontz.one>
2026-02-18 02:15:18 -0300

    feat(auth): add sign-in and sign-up pages with routing

---
+++ app/pages/auth/sign-in/[...all].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth',
+});
+</script>
+
+<template>
+  <SignIn routing="path" path="/auth/sign-in" sign-up-url="/auth/sign-up" />
+</template>
---
+++ app/pages/auth/sign-up/[...all].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth',
+});
+</script>
+
+<template>
+  <SignUp routing="path" path="/auth/sign-up" sign-in-url="/auth/sign-in" />
+</template>
