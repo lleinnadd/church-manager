e78d2559def3e82748e28399f9b8b461674cc478
lleinnadd <daniel@frontz.one>
2026-02-20 00:49:15 -0300

    feat: enable validation on change, input, and model update in vee-validate configuration

--- app/plugins/vee-validate.ts
+++ app/plugins/vee-validate.ts
@@ -3,8 +3,8 @@ import { configure } from 'vee-validate';
 export default defineNuxtPlugin(() => {
   configure({
     validateOnBlur: true,
-    validateOnChange: false,
-    validateOnInput: false,
-    validateOnModelUpdate: false,
+    validateOnChange: true,
+    validateOnInput: true,
+    validateOnModelUpdate: true,
   });
 });
